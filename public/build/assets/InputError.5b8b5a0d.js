6d838a22ba4176c88d31a80bec249239238b84fb
Minh Ngoc <leminhngocdohai@gmail.com>
2022-11-03 01:32:48 +0700

    build

---
+++ public/build/assets/InputError.5b8b5a0d.js
@@ -0,0 +1,1 @@
+import{a as n}from"./app.261230c5.js";function o({message:r,className:t=""}){return r?n("p",{className:"text-sm text-red-600 "+t,children:r}):null}export{o as I};
---
+++ public/build/assets/InputLabel.27d5514c.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app.261230c5.js";function e({forInput:t,value:r,children:n}){return o("label",{htmlFor:t,children:r||n})}export{e as I};
